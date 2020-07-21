e2c17495807e011f9cc224edec0ce27d24c90c7c
IoTKETI <iot.keti@gmail.com>
2020-07-21 05:25:44 +0100

    Init commit

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/sh
 
-sudo chmod 777 /home/pi/nCube-dry-100/
 cd /home/pi/nCube-dry-100
-sudo chmod 777 *
+sudo killall python3
+git stash
+git pull
+sleep 5
+git stash pop
 python3 start.py
 sleep 2
 python3 exec_print.py &
@@ -13,6 +16,4 @@ python3 exec_res2.py &
 sleep 2
 python3 exec_res.py &
 sleep 2
-pm2 start thyme.js
-
-
+pm2 restart thyme.js
